4e60a40f76e19ca531dfb6ce5008b3fe9182ba8c
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-25 11:55:10 +0000

    Update lemp-setup.sh
    Unit testing added

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo touch test.txt # Unit tests log file for LEMP
 sudo apt install git -y
-git clone 
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
+sudo systemctl status nginx > test.txt
 sudo apt install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+sudo systemctl status mariadb >> test.txt
 sudo apt install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
- 
+php -v >> test.txt

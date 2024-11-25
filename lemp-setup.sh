c15c070ab1a372713a4b8be35afa821ccd7f9cd0
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-25 12:18:14 +0000

    Update lemp-setup.sh
    v1.3

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo touch test.txt # Unit tests log file for LEMP
+sudo touch /root/test.txt # Unit tests log file for LEMP
 sudo apt install git -y
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo systemctl status nginx > test.txt
+sudo systemctl status nginx > /root/test.txt
 sudo apt install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-sudo systemctl status mariadb >> test.txt
+sudo systemctl status mariadb >> /root/test.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
-sudo php -v >> test.txt
+sudo php -v >> /root/test.txt

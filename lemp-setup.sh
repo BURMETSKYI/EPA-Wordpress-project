df345209d0c7dcb9e8a8f5504f951e8a64ce77b0
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-25 11:35:05 +0000

    Update lemp-setup.sh
    v1.2

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -3,5 +3,10 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt install git -y
 git clone 
 sudo apt install nginx -y
-systemctl start nginx
-systemctl enable nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+sudo apt install mariadb-server -y
+sudo systemctl start mariadb
+sudo systemctl enable mariadb
+sudo apt install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
+ 

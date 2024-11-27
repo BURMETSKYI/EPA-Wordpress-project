0d53df243babc0197d6d0c7400759998a470ec85
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 13:17:45 +0000

    Update lemp-setup.sh
    wordpress-install.sh >> test.txt removed

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -10,4 +10,4 @@ sudo systemctl status mariadb >> /root/test.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/test.txt
 sudo mv /var/www/html/index.html /var/www/html/index.html.old
-sudo bash /root/EPA-Wordpress-project/wordpress-install.sh >> /root/test.txt
+sudo bash /root/EPA-Wordpress-project/wordpress-install.sh

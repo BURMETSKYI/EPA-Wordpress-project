2272b5d9e3fe738d19613d07341aae4a0b10e555
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 11:19:59 +0000

    Update wordpress-install.sh
    test

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -3,16 +3,14 @@ sudo cd /var/www/
 sudo rm -rf html
 sudo apt -y install unzip
 sudo wget https://wordpress.org/latest.zip
-sudo mkdir -p /usr/share/nginx
 sudo unzip latest.zip
-sudo mv wordpress html
 sudo rm latest.zip
-sudo mariadb -u root
+sudo mv wordpress html
 sudo mysql -e "CREATE DATABASE IF NO EXISTS wordpress"
 sudo mysql -e "CREATE USER wpuser@localhost identified by 'password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo cd /var/www/html
+sudo cd /var/www/html/wordpress
 sudo wget https://wp-s3-storage.s3.us-east-1.amazonaws.com/wp-config.php
 sudo chmod 640 wp-config.php 
 sudo chown -R www-data:www-data /var/www/html

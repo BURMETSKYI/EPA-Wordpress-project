f09a28cd256d19410e8add6628982b5ce2d10dc4
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 13:15:41 +0000

    Update git-setup.sh
    test.txt renamed > nginx_mariadb_php_test.txt

--- git-setup.sh
+++ git-setup.sh
@@ -3,5 +3,5 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git /root/EPA-Wordpress-project
 sudo chmod -R 755 /root/EPA-Wordpress-project/
-sudo touch /root/test.txt # Unit tests log file
-sudo bash /root/EPA-Wordpress-project/lemp-setup.sh >> /root/test.txt
+sudo touch /root/nginx_mariadb_php_test.txt # Unit tests log file
+sudo bash /root/EPA-Wordpress-project/lemp-setup.sh

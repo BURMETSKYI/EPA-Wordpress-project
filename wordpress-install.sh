dd28869c3c70c98e963a3f72ab46a005a841c5c7
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 13:06:49 +0000

    Update wordpress-install.sh
    redundant "cd" command fixed

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -9,7 +9,6 @@ sudo mysql -e "CREATE DATABASE IF NO EXISTS wordpress"
 sudo mysql -e "CREATE USER wpuser@localhost identified by 'password'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo cd /var/www/html/wordpress
 sudo wget -O /var/www/html/wp-config.php https://wp-s3-storage.s3.us-east-1.amazonaws.com/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html

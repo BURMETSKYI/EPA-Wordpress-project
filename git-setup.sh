8363fe004e605b2b1e945c7e6fe03d65d11114b1
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 09:14:15 +0000

    Update git-setup.sh
    update path for git clone to /root

--- git-setup.sh
+++ git-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt -y install git
-sudo cd /root/
-sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git
+sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git /root
 sudo chmod -R 755 /EPA-Wordpress-project/
 sudo bash /EPA-Wordpress-project/lemp-setup.sh

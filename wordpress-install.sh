7b49ae8ed3807239332f26cc546136a2b7b7c2f8
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 15:03:59 +0000

    Update wordpress-install.sh
    fixing of mistypings

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -5,9 +5,9 @@ sudo wget -O /var/www/latest.zip https://wordpress.org/latest.zip
 sudo unzip /var/www/latest.zip -d /var/www
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html
-sudo mysql -e "CREATE DATABASE IF NO EXISTS wordpress"
-sudo mysql -e "CREATE USER wpuser@localhost identified by 'password'"
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost'"
 sudo mysql -e "FLUSH PRIVILEGES"
 sudo wget -O /var/www/html/wp-config.php https://wp-s3-storage.s3.us-east-1.amazonaws.com/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 

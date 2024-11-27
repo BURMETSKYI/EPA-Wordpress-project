bd9608f64a66192f0197f14081c135791bd19270
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 13:18:20 +0000

    Update lemp-setup.sh
    test.txt renamed nginx_mariadb_php_test.txt

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -2,12 +2,12 @@
 sudo apt install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
-sudo systemctl status nginx >> /root/test.txt
+sudo systemctl status nginx >> /root/nginx_mariadb_php_test.txt
 sudo apt install mariadb-server -y
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
-sudo systemctl status mariadb >> /root/test.txt
+sudo systemctl status mariadb >> /root/nginx_mariadb_php_test.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
-sudo php -v >> /root/test.txt
+sudo php -v >> /root/nginx_mariadb_php_test.txt
 sudo mv /var/www/html/index.html /var/www/html/index.html.old
 sudo bash /root/EPA-Wordpress-project/wordpress-install.sh

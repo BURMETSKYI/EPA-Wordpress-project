f1c8597846beee3277aa036752d30371d1f07ae6
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 11:30:05 +0000

    Update git-setup.sh
    direction for script has been updated

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt -y install git
-sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git /root
+sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git EPA-Wordpress-project
 sudo chmod -R 755 /EPA-Wordpress-project/
-sudo bash /root/EPA-Wordpress-project/lemp-setup.sh
+sudo bash /EPA-Wordpress-project/lemp-setup.sh

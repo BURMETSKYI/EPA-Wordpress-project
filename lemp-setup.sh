b62c8efbaac93af8a8403d8e8e0547cb9de7e105
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 14:27:43 +0000

    Update lemp-setup.sh
    mv nginx.conf to /etc/nginx/conf.d/ directory
    added the script for replacing SERVERNAME to $dns_record

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -10,4 +10,8 @@ sudo systemctl status mariadb >> /root/nginx_mariadb_php_test.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/nginx_mariadb_php_test.txt
 sudo mv /var/www/html/index.html /var/www/html/index.html.old
+sudo mv /root/EPA-Wordpress-project/nginx.conf /etc/nginx/conf.d/nginx.conf
+dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')
+sed -i "s/SERVERNAME/$dns_record/g" /etc/nginx/conf.d/nginx.conf
+nginx -t && systemctl reload nginx
 sudo bash /root/EPA-Wordpress-project/wordpress-install.sh

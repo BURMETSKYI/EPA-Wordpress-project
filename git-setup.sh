c756ee6a9c8a2db854d082895bdd42ad4e7dcf53
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-27 12:18:53 +0000

    Update git-setup.sh
    sudo touch /root/test.txt # Unit tests log file

--- git-setup.sh
+++ git-setup.sh
@@ -2,4 +2,5 @@
 sudo apt -y install git
 sudo git clone https://github.com/BURMETSKYI/EPA-Wordpress-project.git /root/EPA-Wordpress-project
 sudo chmod -R 755 /root/EPA-Wordpress-project/
-sudo bash /root/EPA-Wordpress-project/lemp-setup.sh > /root/test.txt
+sudo touch /root/test.txt # Unit tests log file
+sudo bash /root/EPA-Wordpress-project/lemp-setup.sh >> /root/test.txt

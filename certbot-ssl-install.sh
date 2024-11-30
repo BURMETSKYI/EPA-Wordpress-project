7e9934e27ea5acb514a687eccd94955a087dd63d
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-30 21:41:18 +0000

    Update certbot-ssl-install.sh
    variables updated

--- certbot-ssl-install.sh
+++ certbot-ssl-install.sh
@@ -6,18 +6,14 @@ sudo apt upgrade -y
 sudo apt install -y certbot python3-certbot-nginx
 
 # Define your email
-EMAIL=REPLACE_EMAIL_HERE
-DOMAIN=REPLACE_DOMAIN_HERE
+EMAIL=EMAIL
+DOMAIN=DOMAIN
 
 # Use Certbot to obtain and install the SSL certificate
 sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN
 
 # Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
 sudo nginx -t && systemctl reload nginx
-
-# Run the WordPress installation scrip
-
-# sudo bash /root/wordpress-project/wordpress-install.sh
 
 # command to test the installation of new certs. You can only install 50 per week per domain.
 # sudo certbot renew --dry-run

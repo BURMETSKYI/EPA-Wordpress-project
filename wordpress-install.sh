56bbe9dd804648580b54749657d7760b4c6fb35e
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-30 21:37:18 +0000

    Update wordpress-install.sh

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -5,10 +5,23 @@ sudo wget -O /var/www/latest.zip https://wordpress.org/latest.zip
 sudo unzip /var/www/latest.zip -d /var/www
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-sudo mysql -e "CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'password'"
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost'"
+
+# Generate username and password for MariaDB
+password=$(tr -dc 'A-Za-z0-9' < /dev/urandom | head -c 25)
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
+
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+sudo mysql -e "CREATE USER '$username'@'localhost' IDENTIFIED BY '$password'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO '$username'@'localhost'"
 sudo mysql -e "FLUSH PRIVILEGES"
+
 sudo wget -O /var/www/html/wp-config.php https://wp-s3-storage.s3.us-east-1.amazonaws.com/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html
+
+sed -i 's/passwrod_here/$password/g' /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php

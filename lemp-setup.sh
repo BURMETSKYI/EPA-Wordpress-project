a8a582ea2f5d091d4e2922914333e3ee316e1d30
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2024-11-30 12:28:25 +0000

    Update lemp-setup.sh
    apache2 disable

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -9,6 +9,8 @@ sudo systemctl enable mariadb
 sudo systemctl status mariadb >> /root/nginx_mariadb_php_test.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/nginx_mariadb_php_test.txt
+sudo systemctl stop apache2
+sudo systemctl disable apache2
 sudo mv /var/www/html/index.html /var/www/html/index.html.old
 sudo mv /root/EPA-Wordpress-project/nginx.conf /etc/nginx/conf.d/nginx.conf
 dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')

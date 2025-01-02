a295044e798fdc4f691f08b62923fc59cd495175
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2025-01-02 00:31:27 +0000

    Update wordpress-install.sh
    mariadb-server installation

--- dev/wordpress-install.sh
+++ dev/wordpress-install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Constants
-rds_edpoint=RDS_ENDPOINT  # Replace with RDS endpoint
+# rds_edpoint=RDS_ENDPOINT  # Replace with RDS endpoint
 db_username=DB_USERNAME   # Replace with RDS admin username
 db_password=DB_PASSWORD   # Replace with RDS admin password
-storage_url=STORAGE_URL   # Replace with persistent storage url
 
 # Functions for error handling
 function check_exit_status {
@@ -40,9 +39,9 @@ sudo rm /var/www/latest.zip
 check_exit_status "WordPress setup"
 
 # Create or connect to existing database in RDS
-echo "Creating database '$db_username' on RDS..."
-mysql -h $rds_edpoint -u $db_username -p$db_password -e "CREATE DATABASE IF NOT EXISTS $db_username;"
-check_exit_status "Database creation"
+# echo "Creating database '$db_username' on RDS..."
+# mysql -h $rds_edpoint -u $db_username -p$db_password -e "CREATE DATABASE IF NOT EXISTS $db_username;"
+# check_exit_status "Database creation"
 
 # Set up wp-config.php
 echo "Configuring WordPress..."
@@ -50,7 +49,7 @@ sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sudo sed -i "s/database_name_here/$db_username/g" /var/www/html/wp-config.php
 sudo sed -i "s/username_here/$db_username/g" /var/www/html/wp-config.php
 sudo sed -i "s/password_here/$db_password/g" /var/www/html/wp-config.php
-sudo sed -i "s/localhost/$rds_edpoint/g" /var/www/html/wp-config.php
+# sudo sed -i "s/localhost/$rds_edpoint/g" /var/www/html/wp-config.php
 
 # Secure the wp-config.php file
 echo "Securing WordPress configuration..."
@@ -59,14 +58,4 @@ sudo chown -R www-data:www-data /var/www/html
 check_exit_status "WordPress configuration security"
 
 echo "WordPress setup is complete. You can now visit your site."
-
-# S3fuse mount of persistent content storage
-echo "Setting up persistent storage..."
-mkdir -p /home/ubuntu/s3-epa
-sudo chown ubuntu:ubuntu /home/ubuntu/s3-epa
-sudo chmod 755 /home/ubuntu/s3-epa
-sudo apt install s3fs -y
-sudo sed -i 's/^#user_allow_other/user_allow_other/' /etc/fuse.conf
-s3fs s3-epa /home/ubuntu/s3-epa -o iam_role=auto -o endpoint=eu-west-2 -o url=$storage_url -o allow_other -o use_path_request_style -o nonempty
-check_exit_status "S3 storage mount"
 

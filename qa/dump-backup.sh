fc60cf33e9c97da14fe3f69c12ccb6c6b53815a3
Vadym Burmetskyi <131384006+BURMETSKYI@users.noreply.github.com>
2025-01-04 21:33:25 +0000

    Update dump-backup.sh

--- qa/dump-backup.sh
+++ qa/dump-backup.sh
@@ -7,14 +7,14 @@ db_username=DB_USERNAME
 db_password=DB_PASSWORD
 domain=DOMAIN
 s_domain=S_DOMAIN
-backup_file="/home/ubuntu/wp_backup.sql"
+backup_file="/home/ubuntu/"
 
 # Create backup
 echo "Creating database backup..."
 mysqldump -h "$rds_endpoint" \
 -u "$db_username" \
 -p"$db_password" \
---databases admin > "$backup_file"
+--databases admin > "$backup_file/wp_backup.sql"
 
 # Set permissions
 echo "Setting file permissions..."
@@ -23,15 +23,15 @@ sudo chown ubuntu:ubuntu "$backup_file"
 
 # Copy to S3 mount to dev
 echo "Copying to S3 mount point..."
-sudo cp "$backup_file" /home/ubuntu/s3-epa/dev/
+sudo cp "$backup_file/wp_backup.sql" /home/ubuntu/s3-epa/dev/wp_backup_$domain.sql
 
 # Replace URLs
 echo "Replacing URLs in backup file..."
-sed -i 's|https://$domain|https://$s_domain|g' "$backup_file"
+sed -i 's|https://$domain|https://$s_domain|g' "$backup_file/wp_backup.sql"
 
 # Copy to S3 mount for prod
 echo "Copying to S3 mount point..."
-sudo cp "$backup_file" /home/ubuntu/s3-epa/
+sudo cp "$backup_file/wp_backup.sql" /home/ubuntu/s3-epa/wp_backup_$s_domain.sql
 
 sudo rm $backup_file
 
